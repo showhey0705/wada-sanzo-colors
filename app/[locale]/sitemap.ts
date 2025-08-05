2b9706b880d0733e94ec83fdcd64eb17766861ce
Shohei <showhey0705@gmail.com>
2025-08-05 09:04:31 +0900

    ページ表示問題の修正

---
+++ app/[locale]/sitemap.ts
@@ -0,0 +1,56 @@
+import { colorsWithSlug } from "@/data/colors";
+import { createCombinationArray } from "@/utils/helper";
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const urls: MetadataRoute.Sitemap = [
+    {
+      url: "https://www.wada-sanzo-colors.com",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://www.wada-sanzo-colors.com/about",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.wada-sanzo-colors.com/inspiration",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.9,
+    },
+    ...colorsWithSlug.map((c) => ({
+      url: `https://www.wada-sanzo-colors.com/colors/${c.slug}`,
+      lastModified: new Date(),
+      changeFrequency: "never" as
+        | "yearly"
+        | "always"
+        | "hourly"
+        | "daily"
+        | "weekly"
+        | "monthly"
+        | "never"
+        | undefined,
+      priority: 0.5,
+    })),
+    ...createCombinationArray(colorsWithSlug).map((c) => ({
+      url: `https://www.wada-sanzo-colors.com/combinations/${c.id}`,
+      lastModified: new Date(),
+      changeFrequency: "never" as
+        | "yearly"
+        | "always"
+        | "hourly"
+        | "daily"
+        | "weekly"
+        | "monthly"
+        | "never"
+        | undefined,
+      priority: 0.5,
+    })),
+  ];
+
+  return urls;
+}
